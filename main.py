983a7d99a5e66890a486b69e6ecc6bfa7cd9af26
Eramcheg <eramcheg@gmail.com>
2022-12-24 20:58:12 +0100

    version with gui, instructions is shown

--- main.py
+++ main.py
@@ -3,97 +3,193 @@ import openpyxl
 from PIL import Image
 import os
 from tkinter import filedialog
-
+from openpyxl.styles import Alignment
+from tkinter import ttk
 
 
 class MainClass:
     def __init__(self):
-        self.filepath = 'Test.xlsx'
+        self.filepath = ""
         self.dirImages = []
-        self.imagePath = ""
+        self.columnArticles = 'B'
+        self.columnImages = 'A'
 
     def startApp(self):
         root = tk.Tk()
+        alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                    'U', 'V', 'W', 'X', 'Y', 'Z', 'AA', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AJ', 'AK',
+                    'AL', 'AM', 'AN', 'AO', 'AP', 'AQ', 'AR', 'AS', 'AT', 'AU', 'AV', 'AW', 'AX', 'AY', 'AZ', 'BA',
+                    'BB', 'BC', 'BD', 'BE', 'BF', 'BG', 'BH', 'BI', 'BJ', 'BK', 'BL', 'BM', 'BN', 'BO', 'BP', 'BQ',
+                    'BR', 'BS', 'BT', 'BU', 'BV', 'BW', 'BX', 'BY', 'BZ', 'CA', 'CB', 'CC', 'CD', 'CE', 'CF', 'CG',
+                    'CH', 'CI', 'CJ', 'CK', 'CL', 'CM', 'CN', 'CO', 'CP', 'CQ', 'CR', 'CS', 'CT', 'CU', 'CV', 'CW',
+                    'CX', 'CY', 'CZ', 'DA', 'DB', 'DC', 'DD', 'DE', 'DF', 'DG', 'DH', 'DI', 'DJ', 'DK', 'DL', 'DM',
+                    'DN', 'DO', 'DP', 'DQ', 'DR', 'DS', 'DT', 'DU', 'DV', 'DW', 'DX', 'DY', 'DZ', 'EA', 'EB', 'EC',
+                    'ED', 'EE', 'EF', 'EG', 'EH', 'EI', 'EJ', 'EK', 'EL', 'EM', 'EN', 'EO', 'EP', 'EQ', 'ER', 'ES',
+                    'ET', 'EU', 'EV', 'EW', 'EX', 'EY', 'EZ', 'FA', 'FB', 'FC', 'FD', 'FE', 'FF', 'FG', 'FH', 'FI',
+                    'FJ', 'FK', 'FL', 'FM', 'FN', 'FO', 'FP', 'FQ', 'FR', 'FS', 'FT', 'FU', 'FV', 'FW', 'FX', 'FY',
+                    'FZ', 'GA', 'GB', 'GC', 'GD', 'GE', 'GF', 'GG', 'GH', 'GI', 'GJ', 'GK', 'GL', 'GM', 'GN', 'GO',
+                    'GP', 'GQ', 'GR', 'GS', 'GT', 'GU', 'GV', 'GW', 'GX', 'GY', 'GZ', 'HA', 'HB', 'HC', 'HD', 'HE',
+                    'HF', 'HG', 'HH', 'HI', 'HJ', 'HK', 'HL', 'HM', 'HN', 'HO', 'HP', 'HQ', 'HR', 'HS', 'HT', 'HU',
+                    'HV', 'HW', 'HX', 'HY', 'HZ', 'IA', 'IB', 'IC', 'ID', 'IE', 'IF', 'IG', 'IH', 'II', 'IJ', 'IK',
+                    'IL', 'IM', 'IN', 'IO', 'IP', 'IQ', 'IR', 'IS', 'IT', 'IU', 'IV', 'IW', 'IX', 'IY', 'IZ', 'JA',
+                    'JB', 'JC', 'JD', 'JE', 'JF', 'JG', 'JH', 'JI', 'JJ', 'JK', 'JL', 'JM', 'JN', 'JO', 'JP', 'JQ',
+                    'JR', 'JS', 'JT', 'JU', 'JV', 'JW', 'JX', 'JY', 'JZ', 'KA', 'KB', 'KC', 'KD', 'KE', 'KF', 'KG',
+                    'KH', 'KI', 'KJ', 'KK', 'KL', 'KM', 'KN', 'KO', 'KP', 'KQ', 'KR', 'KS', 'KT', 'KU', 'KV', 'KW',
+                    'KX', 'KY', 'KZ', 'LA', 'LB', 'LC', 'LD', 'LE', 'LF', 'LG', 'LH', 'LI', 'LJ', 'LK', 'LL', 'LM',
+                    'LN', 'LO', 'LP', 'LQ', 'LR', 'LS', 'LT', 'LU', 'LV', 'LW', 'LX', 'LY', 'LZ', 'MA', 'MB', 'MC',
+                    'MD', 'ME', 'MF', 'MG', 'MH', 'MI', 'MJ', 'MK', 'ML', 'MM', 'MN', 'MO', 'MP', 'MQ', 'MR', 'MS',
+                    'MT', 'MU', 'MV', 'MW', 'MX', 'MY', 'MZ', 'NA', 'NB', 'NC', 'ND', 'NE', 'NF', 'NG', 'NH', 'NI',
+                    'NJ', 'NK', 'NL', 'NM', 'NN', 'NO', 'NP', 'NQ', 'NR', 'NS', 'NT', 'NU', 'NV', 'NW', 'NX', 'NY',
+                    'NZ', 'OA', 'OB', 'OC', 'OD', 'OE', 'OF', 'OG', 'OH', 'OI', 'OJ', 'OK', 'OL', 'OM', 'ON', 'OO',
+                    'OP', 'OQ', 'OR', 'OS', 'OT', 'OU', 'OV', 'OW', 'OX', 'OY', 'OZ', 'PA', 'PB', 'PC', 'PD', 'PE',
+                    'PF', 'PG', 'PH', 'PI', 'PJ', 'PK', 'PL', 'PM', 'PN', 'PO', 'PP', 'PQ', 'PR', 'PS', 'PT', 'PU',
+                    'PV', 'PW', 'PX', 'PY', 'PZ', 'QA', 'QB', 'QC', 'QD', 'QE', 'QF', 'QG', 'QH', 'QI', 'QJ', 'QK',
+                    'QL', 'QM', 'QN', 'QO', 'QP', 'QQ', 'QR', 'QS', 'QT', 'QU', 'QV', 'QW', 'QX', 'QY', 'QZ', 'RA',
+                    'RB', 'RC', 'RD', 'RE', 'RF', 'RG', 'RH', 'RI', 'RJ', 'RK', 'RL', 'RM', 'RN', 'RO', 'RP', 'RQ',
+                    'RR', 'RS', 'RT', 'RU', 'RV', 'RW', 'RX', 'RY', 'RZ', 'SA', 'SB', 'SC', 'SD', 'SE', 'SF', 'SG',
+                    'SH', 'SI', 'SJ', 'SK', 'SL', 'SM', 'SN', 'SO', 'SP', 'SQ', 'SR', 'SS', 'ST', 'SU', 'SV', 'SW',
+                    'SX', 'SY', 'SZ', 'TA', 'TB', 'TC', 'TD', 'TE', 'TF', 'TG', 'TH', 'TI', 'TJ', 'TK', 'TL', 'TM',
+                    'TN', 'TO', 'TP', 'TQ', 'TR', 'TS', 'TT', 'TU', 'TV', 'TW', 'TX', 'TY', 'TZ', 'UA', 'UB', 'UC',
+                    'UD', 'UE', 'UF', 'UG', 'UH', 'UI', 'UJ', 'UK', 'UL', 'UM', 'UN', 'UO', 'UP', 'UQ', 'UR', 'US',
+                    'UT', 'UU', 'UV', 'UW', 'UX', 'UY', 'UZ', 'VA', 'VB', 'VC', 'VD', 'VE', 'VF', 'VG', 'VH', 'VI',
+                    'VJ', 'VK', 'VL', 'VM', 'VN', 'VO', 'VP', 'VQ', 'VR', 'VS', 'VT', 'VU', 'VV', 'VW', 'VX', 'VY',
+                    'VZ', 'WA', 'WB', 'WC', 'WD', 'WE', 'WF', 'WG', 'WH', 'WI', 'WJ', 'WK', 'WL', 'WM', 'WN', 'WO',
+                    'WP', 'WQ', 'WR', 'WS', 'WT', 'WU', 'WV', 'WW', 'WX', 'WY', 'WZ', 'XA', 'XB', 'XC', 'XD', 'XE',
+                    'XF', 'XG', 'XH', 'XI', 'XJ', 'XK', 'XL', 'XM', 'XN', 'XO', 'XP', 'XQ', 'XR', 'XS', 'XT', 'XU',
+                    'XV', 'XW', 'XX', 'XY', 'XZ', 'YA', 'YB', 'YC', 'YD', 'YE', 'YF', 'YG', 'YH', 'YI', 'YJ', 'YK',
+                    'YL', 'YM', 'YN', 'YO', 'YP', 'YQ', 'YR', 'YS', 'YT', 'YU', 'YV', 'YW', 'YX', 'YY', 'YZ', 'ZA',
+                    'ZB', 'ZC', 'ZD', 'ZE', 'ZF', 'ZG', 'ZH', 'ZI', 'ZJ', 'ZK', 'ZL', 'ZM', 'ZN', 'ZO', 'ZP', 'ZQ',
+                    'ZR', 'ZS', 'ZT', 'ZU', 'ZV', 'ZW', 'ZX', 'ZY', 'ZZ']
         root.geometry("600x600")
         root.title("Main")
         root.configure(background="black")
 
-        buttonFrame = tk.Frame(root)
+        buttonFrame = tk.Frame(root, background='light gray')
         buttonFrame.columnconfigure(0, weight=1)
         buttonFrame.columnconfigure(1, weight=1)
-        buttonFrame.pack(pady=200)
 
-        labelExcel = tk.Label(buttonFrame, text=self.filepath)
-        labelExcel.grid(row=0, column=0)
+        buttonFrame.pack(pady=160, padx=0)
 
-        labelImage = tk.Label(buttonFrame, text=self.imagePath)
-        labelImage.grid(row=1, column=0)
+        labelExcel = tk.Label(buttonFrame, text="          File is not selected         ", foreground='red',
+                              font=('Arial', 10))
+        labelExcel.grid(row=1, column=0)
 
-        buttonExcel = tk.Button(buttonFrame, text="Excel file", command=lambda: self.openFile(labelExcel))
-        buttonExcel.grid(row=0, column=1)
+        labelImage = tk.Label(buttonFrame, text="        Folder is not selected       ", foreground='red',
+                              font=('Arial', 10))
+        labelImage.grid(row=3, column=0)
 
-        buttonImage = tk.Button(buttonFrame, text="Image folder", command=lambda: self.openImage(labelImage))
-        buttonImage.grid(row=1, column=1)
+        labelInsert = tk.Label(buttonFrame, text="Click Insert to insert images into excel file", foreground='red',
+                               font=('Arial', 11))
+        labelInsert.grid(row=5, column=0, sticky=tk.E + tk.W, columnspan=2)
 
-        buttonSave = tk.Button(buttonFrame, text="Insert", command=self.saveImage)
-        buttonSave.grid(row=2, column=1)
-        quit = tk.Button(buttonFrame, text="quit", command=self.quitcode)
-        quit.grid(row=3, column=1)
+        buttonExcel = tk.Button(buttonFrame, text="Load excel file", font=('Arial', 17),
+                                command=lambda: self.openFile(labelExcel, labelInsert))
+        buttonExcel.grid(row=0, column=0, sticky=tk.E + tk.W)
+
+        buttonImage = tk.Button(buttonFrame, text="Load image folder", font=('Arial', 17), width=10,
+                                command=lambda: self.openImage(labelImage, labelInsert))
+        buttonImage.grid(row=2, column=0, sticky=tk.E + tk.W)
+
+        buttonSave = tk.Button(buttonFrame, text="Insert", font=('Arial', 19),
+                               command=lambda: self.saveImage(labelInsert))
+        buttonSave.grid(row=4, column=0, sticky=tk.E + tk.W, columnspan=2)
+
+        labelArticles = tk.Label(buttonFrame, text="Select the excel column\n"
+                                                   "where the codes are located", font=('Arial', 11),
+                                 background='light gray')
+        labelArticles.grid(row=0, column=1)
+
+        labelImages = tk.Label(buttonFrame, text="Select the excel column\n"
+                                                 "where the images will be located", font=('Arial', 11),
+                               background='light gray')
+        labelImages.grid(row=2, column=1)
+
+        columnArticles = ttk.Combobox(buttonFrame, values=alphabet, font=('Arial', 12), background='light gray')
+        columnArticles.grid(row=1, column=1)
+        columnArticles.set("Column with Numbers")
+
+        columnArticles.bind('<<ComboboxSelected>>', self.articlesChanged)
+
+        columnImages = ttk.Combobox(buttonFrame, values=alphabet, font=('Arial', 12), background='black')
+        columnImages.set("Column for images")
+        columnImages.grid(row=3, column=1)
+
+        columnImages.bind('<<ComboboxSelected>>', self.imagesChanged)
+
+        quit = tk.Button(buttonFrame, text="Quit", font=('Arial', 19), command=self.quitcode)
+        quit.grid(row=6, column=0, sticky=tk.E + tk.W, columnspan=2)
 
         root.mainloop()
 
-    def openFile(self, label):
+    def articlesChanged(self, event):
+        self.columnArticles = event.widget.get()
+        print(self.columnArticles)
+
+    def imagesChanged(self, event):
+        self.columnImages = event.widget.get()
+        print(self.columnImages)
+
+    def openFile(self, label, labelInsert):
         tempdir = filedialog.askopenfilename(initialdir="/", title="Select An Excel File", filetypes=(
             ("excel files", "*.xlsx"), ("All files", "*.*")))
         if len(tempdir) > 0:
             self.filepath = tempdir
-            label.configure(text=tempdir)
+            label.configure(text="      File has been selected      ", foreground='green')
+            labelInsert.configure(text="Click Insert to insert images into excel file", foreground='red',
+                                  font=('Arial', 11))
         label.update()
+        labelInsert.update()
         print(tempdir)
 
-    def openImage(self, label):
+    def openImage(self, label, labelInsert):
         tempdir = filedialog.askdirectory(initialdir="/", title="Select An Image Folder")
-
         if len(tempdir) > 0:
             self.imagePath = tempdir
-            label.configure(text=tempdir)
-        label.update()
-        self.dirImages = os.listdir(tempdir)
-        self.imagePath = tempdir
-        print(tempdir)
+            label.configure(text="    Folder has been selected    ", foreground='green')
+            labelInsert.configure(text="Click Insert to insert images into excel file", foreground='red',
+                                  font=('Arial', 11))
+            label.update()
+            labelInsert.update()
+            self.dirImages = os.listdir(tempdir)
+            self.imagePath = tempdir
+            print(tempdir)
 
-    def saveImage(self):
+    def saveImage(self, label):
 
-        start = 1
-        finish = len(self.dirImages)
+        if self.filepath != '' and self.dirImages != []:
+            try:
+                start = 1
+                finish = len(self.dirImages)
+                for i in range(start, finish + 1):
+                    workbook = openpyxl.load_workbook(self.filepath)                        # opening workbook
+                    worksheet = workbook["List1"]                                           # opening sheet 1
+                    artical_number = str(worksheet[self.columnArticles + str(i)].value)     # unique number of
+                                                                                            # product in cell
 
-        for i in range(start, finish + 1):
+                    for j in range(finish):
+                        if artical_number in self.dirImages[j]:
+                            img = Image.open(str(self.imagePath + "/" + self.dirImages[j]))  # opening an image
+                            img1 = openpyxl.drawing.image.Image(img)
+                            img1.width = 200 * img1.width / img1.height                      # Set new image size
+                            img1.height = 200
 
-            workbook = openpyxl.load_workbook('Test.xlsx')  # opening workbook
-            worksheet = workbook.get_sheet_by_name("List1") # opening sheet 1
-            artical_number = str(worksheet['B' + str(i)].value) #unique number of product
+                            worksheet.row_dimensions[i].height = int(150)                    # Set row and column
+                            worksheet.column_dimensions[self.columnImages].width = 44.6      # width and height
 
-            for j in range(finish):
-                if artical_number in self.dirImages[j]:
-                    img = Image.open(str(self.imagePath + "/" + self.dirImages[j]))
+                            cell = worksheet[self.columnImages + str(i)]
+                            cell.alignment = Alignment(horizontal='right')
+                            worksheet.add_image(img1, self.columnImages + str(i))            # Adding image to worksheet
 
-                    # img1=Image(img)
+                            workbook.save(self.filepath)                                     # Saving a document
 
-                    # img=img.resize((400,400))
-                    #img.thumbnail((200, 200))
+                            break
+                label.configure(text="File data has been updated!", foreground='green')
+                label.update()
+                print("Program is successful")
 
-                    img1 = openpyxl.drawing.image.Image(img)
-
-                    img1.width = 200*img1.width/img1.height
-                    img1.height = 200
-                    worksheet.add_image(img1, "A" + str(i))
-                    worksheet.row_dimensions[i].height = int(150)
-                    worksheet.column_dimensions["A"].width = 44.6
-                    workbook.save(self.filepath)
-
-                    break
-
-        print("Program is successful")
+            except:
+                label.configure(text="Something went wrong", foreground='orange')
+                label.update()
 
     def quitcode(self):
         quit()
--- testfolders.py
+++ testfolders.py
@@ -1,4 +1,9 @@
 import os
 
 arr = os.listdir("G:/FIles/test")
-print(arr)
+alphabet= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+alphabet2=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+for i in (alphabet):
+    for j in alphabet:
+        alphabet2.append(i+j)
+print(alphabet2)
