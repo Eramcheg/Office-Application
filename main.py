b60ee8f588eebf5341df85f635d437a6a71dc5c7
Eramcheg <eramcheg@gmail.com>
2022-12-24 10:32:19 +0100

    working folder upload

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import tkinter as tk
 import openpyxl
+from PIL import Image
 import os
 from tkinter import filedialog
+
+
+
 class MainClass:
     def __init__(self):
-        self.filepath=""
-        self.imagePath=""
+        self.filepath = 'Test.xlsx'
+        self.dirImages = []
+        self.imagePath = ""
+
     def startApp(self):
         root = tk.Tk()
         root.geometry("600x600")
@@ -16,53 +22,82 @@ class MainClass:
         buttonFrame.columnconfigure(0, weight=1)
         buttonFrame.columnconfigure(1, weight=1)
         buttonFrame.pack(pady=200)
+
         labelExcel = tk.Label(buttonFrame, text=self.filepath)
         labelExcel.grid(row=0, column=0)
+
         labelImage = tk.Label(buttonFrame, text=self.imagePath)
         labelImage.grid(row=1, column=0)
-        but=tk.Button(buttonFrame,text="Excel file", command=lambda:self.openFile(labelExcel))
-        but.grid(row=0, column=1)
-        but1 = tk.Button(buttonFrame,text="Image file", command=lambda:self.openImage(labelImage))
-        but1.grid(row=1, column=1)
-        quit=tk.Button(buttonFrame,text="quit", command=self.quitcode)
-        quit.grid(row=2,column=1)
+
+        buttonExcel = tk.Button(buttonFrame, text="Excel file", command=lambda: self.openFile(labelExcel))
+        buttonExcel.grid(row=0, column=1)
+
+        buttonImage = tk.Button(buttonFrame, text="Image folder", command=lambda: self.openImage(labelImage))
+        buttonImage.grid(row=1, column=1)
+
+        buttonSave = tk.Button(buttonFrame, text="Insert", command=self.saveImage)
+        buttonSave.grid(row=2, column=1)
+        quit = tk.Button(buttonFrame, text="quit", command=self.quitcode)
+        quit.grid(row=3, column=1)
+
         root.mainloop()
 
-    def search_for_file_path(self):
-        currdir = os.getcwd()
-        tempdir = filedialog.askdirectory( initialdir=currdir, title='Please select a path to excel file')
-        if len(tempdir) > 0:
-            print("You chose: %s" % tempdir)
-        print(tempdir)
     def openFile(self, label):
         tempdir = filedialog.askopenfilename(initialdir="/", title="Select An Excel File", filetypes=(
-        ("excel files", "*.xlsx"), ("All files", "*.*")))
-
+            ("excel files", "*.xlsx"), ("All files", "*.*")))
         if len(tempdir) > 0:
-            self.filepath=tempdir
+            self.filepath = tempdir
             label.configure(text=tempdir)
         label.update()
         print(tempdir)
-        #fileName=filedialog.askopenfilename(initialdir="/",title="select a file", filetype=(("jpeg", "*.jpg")))
 
-    def openImage(self,label):
-        tempdir = filedialog.askopenfilename(initialdir="/", title="Select An Excel File", filetypes=(
-            (("jpeg files", "*.jpg"), ("png files", "*.png"))))
+    def openImage(self, label):
+        tempdir = filedialog.askdirectory(initialdir="/", title="Select An Image Folder")
 
         if len(tempdir) > 0:
             self.imagePath = tempdir
             label.configure(text=tempdir)
         label.update()
+        self.dirImages = os.listdir(tempdir)
+        self.imagePath = tempdir
         print(tempdir)
+
+    def saveImage(self):
+
+        start = 1
+        finish = len(self.dirImages)
+
+        for i in range(start, finish + 1):
+
+            workbook = openpyxl.load_workbook('Test.xlsx')  # opening workbook
+            worksheet = workbook.get_sheet_by_name("List1") # opening sheet 1
+            artical_number = str(worksheet['B' + str(i)].value) #unique number of product
+
+            for j in range(finish):
+                if artical_number in self.dirImages[j]:
+                    img = Image.open(str(self.imagePath + "/" + self.dirImages[j]))
+
+                    # img1=Image(img)
+
+                    # img=img.resize((400,400))
+                    #img.thumbnail((200, 200))
+
+                    img1 = openpyxl.drawing.image.Image(img)
+
+                    img1.width = 200*img1.width/img1.height
+                    img1.height = 200
+                    worksheet.add_image(img1, "A" + str(i))
+                    worksheet.row_dimensions[i].height = int(150)
+                    worksheet.column_dimensions["A"].width = 44.6
+                    workbook.save(self.filepath)
+
+                    break
+
+        print("Program is successful")
 
     def quitcode(self):
         quit()
-m=MainClass()
+
+
+m = MainClass()
 m.startApp()
-
-
-
-
-
-
-
